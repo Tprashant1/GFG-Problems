553d291036e20f568825bdd40f2392245deb4200
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2023-06-26 12:08:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Leaders in an array/leaders-in-an-array.py
@@ -0,0 +1,49 @@
+class Solution:
+    #Back-end complete function Template for Python 3
+    
+    #Function to find the leaders in the array.
+    def leaders(self, A, N):
+        #Code here
+        A.reverse()
+        mx=A[0]
+        l=[]
+        l.append(A[0])
+        for i in range(1,N):
+            if mx <= A[i]:
+                l.append(A[i])
+                mx = A[i]
+        l.reverse()
+        return l
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().strip().split()]
+        obj = Solution()
+        
+        A=obj.leaders(A,N)
+        
+        for i in A:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
