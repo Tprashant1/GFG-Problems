3e26b30547d9c370eb81c1a9da68f2060a7099e0
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2024-09-08 14:43:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        if(arr[0] == 0){
+            return -1;
+        }
+        
+        int n = arr.size();
+        int maxjump = 0; 
+        int limit = 0;
+        int ans = 0;
+        for(int i=0;i<n-1;i++){
+            maxjump = max(maxjump,i+arr[i]);
+            if(i == limit){
+                limit = maxjump;
+                ans++;
+            }
+        }
+        
+        if(limit < n-1){
+            return -1;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, i, j;
+        vector<int> arr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.minJumps(arr) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
