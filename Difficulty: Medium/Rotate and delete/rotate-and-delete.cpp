a286a8983334685479e095908fbf0f79ff51b776
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2024-10-02 18:27:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate and delete/rotate-and-delete.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int rotateDelete(vector<int> &arr) {
+        int n = arr.size();
+        int k = 0 ;
+        while(k < n/2){  
+            int num = arr[arr.size()-1]; 
+            arr.insert(arr.begin() + 0,num);
+            arr.pop_back();  
+            
+            arr.erase(arr.begin() + arr.size()-k- 1); 
+            k++;
+        }
+        return arr[0];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.rotateDelete(arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
