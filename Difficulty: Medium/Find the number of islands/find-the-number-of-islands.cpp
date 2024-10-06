420c95fc8eefd3504eed5445cddaee1fc2bda7ac
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2024-10-06 12:00:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -7,15 +7,18 @@ class Solution {
   public:
     // Function to find the number of islands.
     
-    void DFS(int i, int j, int n, int m,vector<int> &newRow,vector<int> &newCol,vector<vector<bool>>&vis,vector<vector<char>>&grid){
+    void DFS(int i, int j, int n, int m,vector<vector<bool>>&vis,vector<vector<char>>&grid){
         vis[i][j] = true;
+        
+        vector<int>newRow = {-1,-1,0,1,1,1,0,-1};
+        vector<int>newCol = {0,1,1,1,0,-1,-1,-1};
         
         for(int k=0;k<8;k++){
             int x = i + newRow[k];
             int y = j + newCol[k];
             
             if(x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == '1' && !vis[x][y]){
-                DFS(x,y,n,m,newRow,newCol,vis,grid);
+                DFS(x,y,n,m,vis,grid);
             }
         }
     }
@@ -27,20 +30,19 @@ class Solution {
         int ans = 0;
         vector<vector<bool>>vis(n,vector<bool>(m,false));
         
-        vector<int>newRow = {-1,-1,0,1,1,1,0,-1};
-        vector<int>newCol = {0,1,1,1,0,-1,-1,-1};
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == '1' && !vis[i][j]){
                     ans++;
-                    DFS(i,j,n,m,newRow,newCol,vis,grid);
+                    DFS(i,j,n,m,vis,grid);
                 }
             }
         }
         return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
