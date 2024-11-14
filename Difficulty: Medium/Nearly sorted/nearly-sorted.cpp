9f936494111dfd029b49f56d6d8fc73baf736665
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2024-11-14 15:56:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto i:arr){
+            pq.push(i);
+        }
+        
+        int i = 0;
+        while(!pq.empty()){
+            int element = pq.top();
+            pq.pop();
+            arr[i] = element;
+            i++;
+        }
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        obj.nearlySorted(arr, k);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        // cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
