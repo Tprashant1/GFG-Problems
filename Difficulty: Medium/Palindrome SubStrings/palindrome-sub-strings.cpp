b2ef98109796cf265d98011b20d9d58065f982d4
Tprashant1 <112966067+Tprashant1@users.noreply.github.com>
2025-03-09 10:35:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome SubStrings/palindrome-sub-strings.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    
+    bool isPalindrome(string &s,int i,int j,vector<vector<int>> &dp){
+        if(i >= j){
+            return true;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s[i] == s[j]){
+            return dp[i][j] = isPalindrome(s,i+1,j-1,dp);
+        }
+        
+        return dp[i][j] = false;
+    }
+  
+    int countPS(string &s) {
+        int n = s.size();
+        int ans = 0;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(s[i] == s[j]){
+                    if(isPalindrome(s,i,j,dp)){
+                        ans++;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.countPS(s) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
